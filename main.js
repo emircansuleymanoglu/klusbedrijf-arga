b19ce45a63b4b5e66d1f750fd3735965f5c2dedb
emircansuleymanoglu <emircansuleymanoglu@gmail.com>
2026-02-15 20:54:53 +0100

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("ARGA KLUS Premium Website Loaded");
+window.addEventListener("scroll", function() {
+  const elements = document.querySelectorAll(".fade-in");
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    const screen = window.innerHeight;
+    if(position < screen - 100){
+      el.style.opacity = 1;
+      el.style.transform = "translateY(0)";
+    }
+  });
+});
